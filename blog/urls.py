207294c2561bbf7df05d384489c18ae6c034194c
sentinel <komo.iiitm@gmail.com>
2019-07-05 18:12:11 +0530

    add detail view of each posts and linking title to it

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #for 127.0.0.1:8000
-    path('', views.post_list, name='post_list')
+    #for 127.0.0.1:8000-->local
+    #pinnacle.com --> online
+    path('', views.post_list, name='post_list'),
+
+    #for 127.0.0.1:8000/post/1 --> local
+    #pinnacle.com --> online
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
