125928fdf665ee4e3c90ea805dccb78efd125db0
sentinel <komo.iiitm@gmail.com>
2019-07-07 13:10:47 +0530

    add feature to create new blog posts from frontend.

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
 
     #for 127.0.0.1:8000/post/1 --> local
-    #pinnacle.com --> online
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    #pinnacle.com/post/1 --> online
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
+    #for 127.0.0.1:8000/post/new --> local
+    #pinnacle.com/post/new --> online
+    path('post/new', views.post_new, name='post_new'),
 ]
