254bfc40812b907058c8262e327f7125e7ffc341
sentinel <komo.iiitm@gmail.com>
2019-07-07 17:08:50 +0530

    add feature to edit blogpost from frontend

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     #for 127.0.0.1:8000/post/new --> local
     #pinnacle.com/post/new --> online
     path('post/new', views.post_new, name='post_new'),
+
+    #for 127.0.0.1:8000/post/1/edit --> local
+    #pinnacle.com/post/1/edit --> online
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
 ]
