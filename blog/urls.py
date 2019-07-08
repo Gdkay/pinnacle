67863028449d72d8e2a2ede87ddf45856a4a6404
sentinel <komo.iiitm@gmail.com>
2019-07-08 18:49:56 +0530

    add login functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     #for 127.0.0.1:8000-->local
@@ -25,4 +26,8 @@ urlpatterns = [
     #for 127.0.0.1:8000/post/1/publish --> local
     #pinnacle.com/post/1/publish --> online
     path('post/<int:pk>/publish', views.post_publish, name='post_publish'),
+
+    #for 127.0.0.1:8000/accounts/login --> local
+    #pinnacle.com/accounts/login --> online
+    path('accounts/' , include('django.contrib.auth.urls') ) ,  
 ]
