cb95aefc39d183ba028bb25163e5badc9c5b8214
sentinel <komo.iiitm@gmail.com>
2019-07-08 12:31:27 +0530

    adding feature to make drafts and publish

--- blog/urls.py
+++ blog/urls.py
@@ -17,4 +17,12 @@ urlpatterns = [
     #for 127.0.0.1:8000/post/1/edit --> local
     #pinnacle.com/post/1/edit --> online
     path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
+
+    #for 127.0.0.1:8000/drafts --> local
+    #pinnacle.com/drafts --> online
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+
+    #for 127.0.0.1:8000/post/1/publish --> local
+    #pinnacle.com/post/1/publish --> online
+    path('post/<int:pk>/publish', views.post_publish, name='post_publish'),
 ]
